6a669c1ef3f08669d9da8d3d5e3ac3cd37fd8e62
Aaradhya Garg <aaradhyagarg04@gmail.com>
2025-09-26 07:02:16 +0530

    classes

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,11 +1,12 @@
-// Factory Function
-function PersonMaker(name,age){
-    const person = {
-        name: name,
-        age: age,
-        talk(){
-            console.log(`Hi,my name is ${this.name}`);
-        },
-    };
-    return person;
+// The constructor method is a special method of a class for creating and initialising an object instance of that class
+class Person{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+    talk(){
+        console.log(`Hi,my name is ${this.name}`);
+    }
 }
+let p1 = new Person("adam",25);
+let p2 = new Person("eve",25);
