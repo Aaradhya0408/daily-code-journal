9860a323b8a5d5ca15fe86dd67d0e7ec59bd5fc0
Aaradhya Garg <aaradhyagarg04@gmail.com>
2025-10-02 02:35:43 +0530

    Show Route

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+
+// Set view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {   id: "1a",
+        username: "God's Child",
+        content: "I Love Coding",
+    },
+    {   id: "2b",
+        username: "Aaradhya Garg",
+        content: "Blessed",
+    },
+    {   id:"3c",
+        username: "Krishna Bhakt",
+        content: "Krishna Loves Me!!!",
+    }
+
+];
+
+// Routes
+app.get("/posts", (req, res) => {
+  res.render("index.ejs",{ posts });
+});
+app.get("/posts/new",(req,res) =>{
+  res.render("new.ejs");
+});
+app.post("/posts",(req,res)=>{
+ let {username,content} = req.body;
+ posts.push({username, content});
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p)=> p.id === id);
+  
+  res.render("show.ejs",{post});
+});
+
+
+
+// Start server
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
